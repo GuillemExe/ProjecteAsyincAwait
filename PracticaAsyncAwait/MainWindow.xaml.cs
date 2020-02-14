0f407dbc8d7e6f579cfc51efafc36786fbb107d5
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-02-14 16:57:44 +0100

    primera mota de la practica asyncAwait

---
+++ PracticaAsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace PracticaAsyncAwait
+{
+    public partial class MainWindow
+    {
+        private string _routeExplorerFolder;
+
+        private List<string> _listaDirectories;
+        public MainWindow()
+        {
+            _listaDirectories = new List<string>();
+            InitializeComponent();
+
+            ButtonSecuencial.Content = "Boton 1 Secuencial";
+            ButtonTPS.Content = "Boton 2 TPS";
+
+            #region ---> Eventos botones
+            // Event ButtonSecuencial
+            ButtonSecuencial.Click += (sender, args) =>
+            {
+                LectureFolder();
+                //_listaDirectories.Add("hola");
+                //_listaDirectories.Add("hola1");
+                //_listaDirectories.Add("hola2");
+                //_listaDirectories.Add("hola3");
+                //_listaDirectories.Add("hola4");
+                //_listaDirectories.Add("hola5");
+                ListBoxDirectoriesButton1.ItemsSource = _listaDirectories;
+
+                //ListBoxDirectoriesButton1.ItemsSource = _routeExplorerFolder;
+            };
+            // Event ButtonTPS
+            ButtonTPS.Click += (sender, args) =>
+            {
+
+            };
+            // Event lecture const textbox
+            TextBoxExplorerFolder.TextChanged += (sender, args) =>
+            {
+                _routeExplorerFolder = TextBoxExplorerFolder.Text;
+            };
+
+            #endregion
+
+
+        }
+
+        public void LectureFolder()
+        {
+            foreach (var direcotries in Directory.GetDirectories(_routeExplorerFolder))
+            {
+                _listaDirectories.Add(direcotries);
+            }       
+        }
+    }
+}
---
+++ PracticaAsyncAwait/folders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaAsyncAwait
+{
+    class Folders
+    {
+        public bool finDestino { get; set; }
+        public string[] routes { get; set; }
+
+
+    }
+}
